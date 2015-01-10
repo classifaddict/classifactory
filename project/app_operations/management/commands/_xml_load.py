1967f582e274bb5d6692c1a61d763588869aa0a1
classifaddict <ocollioud@gmail.com>
2015-01-10 21:06:23 +0100

    Improved XML loading

--- project/app_operations/management/commands/_xml_load.py
+++ project/app_operations/management/commands/_xml_load.py
@@ -4,25 +4,39 @@ from django.conf import settings
 from app_tree.models import Element, Attribute, Data
 
 
-def store_element(elt, parent=None):
+except_attr = ['lang', 'ipcLevel', 'priorityOrder']
+except_attr_val = [('entryType', 'K')]
+data_elt = ['text', 'references', 'entryReference', 'noteParagraph']
+
+def store_element(elt, attrs, datas, parent=None):
     e = Element.objects.create(name=elt.tag, parent=parent)
-    e.save()
 
-    for name, value in sorted(elt.items()):
-        if value:
-            a = Attribute.objects.create(element=e, name=name, value=value)
-            a.save()
+    attrs.extend([
+        Attribute(
+            element=e,
+            name=name,
+            value=value
+        ) for name, value in sorted(
+            elt.items()
+        ) if name not in except_attr and value and not (
+            name, value
+        ) in except_attr_val
+    ])
 
-    if elt.tag in ['text', 'references', 'entryReference', 'noteParagraph']:
-        d = Data.objects.create(element=e, lang='en', texts=etree.tostring(elt))
-        d.save()
+    if elt.tag in data_elt:
+        datas.append(Data(element=e, lang='en', texts=etree.tostring(elt)))
     else:
         for child in elt:
-            store_element(child, parent=e)
+            store_element(child, attrs, datas, parent=e)
 
 
 def load(filename):
     parser = etree.XMLParser(remove_blank_text=True)
     tree = etree.parse(os.path.join(settings.DATA_DIR, filename), parser)
     root = tree.getroot()
-    store_element(root)
+
+    attrs = []
+    datas = []
+    store_element(root, attrs=attrs, datas=datas)
+    Attribute.objects.bulk_create(attrs)
+    Data.objects.bulk_create(datas)
