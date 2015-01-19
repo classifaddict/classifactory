3cee7bfd454e9b5f63dfdfff10548ce2fc674844
collioud <collioud@wipo.int>
2015-01-19 15:41:38 +0100

    Release/install mgmt.

--- project/app_main/wsgi.py
+++ project/app_main/wsgi.py
@@ -8,7 +8,9 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
+import sys
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app_main.settings")
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
