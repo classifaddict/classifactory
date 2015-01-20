c50b88b39a7828222877258143b9a664bfc66b1d
collioud <collioud@wipo.int>
2015-01-20 14:23:39 +0100

    Fixed static url in httpd.conf and used url tag in template.

--- project/app_tree/urls.py
+++ project/app_tree/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     url(r'^elements/(?P<pk>[0-9]+)/$', views.element_detail),
     url(r'^elements/fancy/$', views.home, name='home'),
     url(r'^elements/fancy/roots/$', views.element_fancy_roots, name='roots'),
-    url(r'^elements/fancy/ancestors/(?P<pk>[0-9]+)/$', views.element_fancy_ancestors),
-    url(r'^elements/fancy/search/(?P<query>[0-9A-Za-z@= ]+)/$', views.element_fancy_search),
-    url(r'^elements/fancy/(?P<pk>[0-9]+)/$', views.element_fancy_children),
+    url(r'^elements/fancy/ancestors/(?P<pk>[0-9]+)/$', views.element_fancy_ancestors, name='ancestors'),
+    url(r'^elements/fancy/search/(?P<query>[0-9A-Za-z@= ]+)/$', views.element_fancy_search, name='search'),
+    url(r'^elements/fancy/(?P<pk>[0-9]+)/$', views.element_fancy_children, name='children'),
 ]
