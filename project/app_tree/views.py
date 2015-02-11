e8321c2af69836215a215ad8486b6d9d1942083f
classifaddict <ocollioud@gmail.com>
2015-02-11 19:48:56 +0100

    Improved search and diff and search result navigation

--- project/app_tree/views.py
+++ project/app_tree/views.py
@@ -103,28 +103,97 @@ def element_fancy_search(request, query):
     List all ancestors' pk of the first element found.
     """
 
-    if request.method == 'GET':
-        r = TreeNode.objects.only('id')
-        for param in query.split():
-            p = param.split('=')
-            r = r.filter(
-                element__attributes__type__name=p[0],
-                element__attributes__value=p[1]
+    queryset = TreeNode.objects.only('id')
+
+    for param in query.split():
+        if ':' in param:
+            p = param.split(':')
+            if len(p) > 2:
+                break
+            queryset = queryset.filter(
+                dataset__doctype__name=p[0],
+                dataset__name=p[1]
+            )
+        elif '@' in param:
+            # Search specified element by attribute
+            p = param.split('@')
+            if len(p) > 2:
+                continue
+            if p[0] and p[1]:
+                if '=' in p[1]:
+                    a = p[1].split('=')
+                    if len(a) > 2:
+                        continue
+                    if a[0] and a[1]:
+                        # 'e@a=v' Search specified element by attribute name and value
+                        queryset = queryset.filter(
+                            element__type__name=p[0],
+                            element__attributes__type__name=a[0],
+                            element__attributes__value=a[1]
+                        )
+                    elif a[1]:
+                        # 'e@=v' Search specified element by attribute value
+                        queryset = queryset.filter(
+                            element__type__name=p[0],
+                            element__attributes__value=a[1]
+                        )
+                else:
+                    # 'e@a' Search specified element by attribute name
+                    queryset = queryset.filter(
+                        element__type__name=p[0],
+                        element__attributes__type__name=p[1],
+                    )
+            elif p[1]:
+                # Search any element by attribute
+                if '=' in p[1]:
+                    a = p[1].split('=')
+                    if len(a) > 2:
+                        continue
+                    if a[0] and a[1]:
+                        # '@a=v' Search any element by attribute name and value
+                        queryset = queryset.filter(
+                            element__attributes__type__name=a[0],
+                            element__attributes__value=a[1]
+                        )
+                    elif a[1]:
+                        # '@=v' Search any element by attribute value
+                        queryset = queryset.filter(
+                            element__attributes__value=a[1]
+                        )
+                else:
+                    # '@a' Search any element by attribute name
+                    queryset = queryset.filter(
+                        element__attributes__type__name=p[1]
+                    )
+            else:
+                # '@' Search any element with attribute
+                queryset = queryset.filter(
+                    element__attributes__isnull=False
+                )
+        else:
+            # if '=' in p[1]:
+                # 'e=t' Search element by content
+            # else:
+            # 'e' Search specified element by name
+            queryset = queryset.filter(
+                element__type__name=param
             )
 
-        if r.exists():
-            # Lazy ancestors (the ones for which element__type__is_main)
-            # only are returned
-            treenode = r[0]
-            ancestors = treenode.get_ancestors(
-                ascending=True,
-                include_self=True
-            ).filter(element__type__is_main=True)
+    paginator = Paginator(queryset, 5)
+    page = request.GET.get('page')
+    try:
+        diffs = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        diffs = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999),
+        # deliver last page of results.
+        diffs = paginator.page(paginator.num_pages)
 
-            serializer = KeySerializer(ancestors, many=True)
-            return JSONResponse(serializer.data)
-        else:
-            return HttpResponse(status=404)
+    serializer = PaginatedKeySerializer(diffs)
+
+    return JSONResponse(serializer.data)
 
 
 def diffs(request, dataset_name):
