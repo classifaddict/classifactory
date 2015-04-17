51724292c0a7dbd1dd45257b5cdc7f06ca9f3f0a
classifaddict <ocollioud@gmail.com>
2015-04-17 17:22:33 +0200

    Added view without table

--- project/app_tree/urls.py
+++ project/app_tree/urls.py
@@ -5,9 +5,10 @@ urlpatterns = [
     url(r'^elements/$', views.element_list),
     url(r'^elements/(?P<pk>[0-9]+)/$', views.element_detail),
     url(r'^elements/fancy/$', views.home, name='home'),
+    url(r'^elements/fancy/(?P<mode>(table|tree))/$', views.mode, name='mode'),
     url(r'^elements/fancy/roots/$', views.element_fancy_roots, name='roots'),
     url(r'^elements/fancy/ancestors/(?P<pk>[0-9]+)/$', views.element_fancy_ancestors, name='ancestors'),
     url(r'^elements/fancy/search/(?P<query>[0-9A-Za-z@=_:! ]+)/$', views.element_fancy_search, name='search'),
-    url(r'^elements/fancy/(?P<pk>[0-9]+)/$', views.element_fancy_children, name='children'),
+    url(r'^elements/fancy/(?P<mode>(table|tree))/(?P<pk>[0-9]+)/$', views.element_fancy_children, name='children'),
     url(r'^elements/fancy/diffs/(?P<doctype_name>[a-z_]+)/(?P<dataset_name>[0-9]{1,8})/$', views.diffs, name='diffs'),
 ]
